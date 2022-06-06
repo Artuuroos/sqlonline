c8f85e290f8d21b716bc8c7eed47abd098328626
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-06 20:50:25 +0200

    Update Sqlpython.py

--- Sqlpython.py
+++ Sqlpython.py
@@ -29,7 +29,7 @@ cursor = conn.cursor()
 
 
 
-            print('Fehler bei der DB Verbindung !')
+            
 
 #scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive',
         #'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets']
